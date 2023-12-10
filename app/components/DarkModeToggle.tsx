a1d4acb60d57d7234c5b1ef68462dc8cb5e835e3
alaborde29 <laborde.@live.fr>
2023-12-10 20:18:42 +0900

    fixed dark mode toggle button

--- app/components/DarkModeToggle.tsx
+++ app/components/DarkModeToggle.tsx
@@ -1,38 +1,31 @@
 // components/DarkModeToggle.js
 import { IconButton, Switch, Typography } from '@mui/material';
 import { useState, useEffect } from 'react';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const DarkModeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    // Check local storage for the user's preferred theme
     const storedTheme = localStorage.getItem('theme');
     setIsDarkMode(storedTheme === 'dark');
   }, []);
 
-  function toggleDarkMode(): any {
-    const newTheme = isDarkMode ? 'dark' : 'light';
+  function toggleDarkMode() {
+    const newTheme = isDarkMode ? 'light' : 'dark'; 
     setIsDarkMode(!isDarkMode);
     localStorage.setItem('theme', newTheme);
-
-    // Apply the new theme to the HTML element
-    document.documentElement.classList.toggle('dark', isDarkMode);
+    document.documentElement.classList.toggle('dark', !isDarkMode); 
   };
 
   return (
-    <div>
-      
       <DarkModeSwitch 
-        className='my-2'
+      className='mt-2'
         checked={!isDarkMode}
         onChange={toggleDarkMode}
+        moonColor='black'
+        sunColor='white'
       />
-    </div>
-
   );
 };
 
