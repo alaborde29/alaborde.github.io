dd34f98ace4c20c3e808dd0fe315dbcc57f80b7e
alaborde29 <laborde.@live.fr>
2024-05-26 20:43:50 +0900

    add: analytics metter

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AnimatedCursor from 'react-animated-cursor';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }:any) {
   return (
     <>
       <AnimatedCursor />
+      <Analytics/>
       <Component {...pageProps} />
     </>
   );
